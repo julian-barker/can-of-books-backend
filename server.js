87874ca54f8d6426eac1e2a848f569f7e7918897
Julian Barker <julian.c.barker@gmail.com>
2022-10-03 16:44:57 -0700

    complete lab11

--- server.js
+++ server.js
@@ -3,16 +3,28 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 
+mongoose.connect(process.env.DB_URL); // DB url in our .env goes here
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongo connection error'));
+db.once('open', function() {
+    console.log('Mongoose is connected to mongo');
+});
+
+const getBooks = require('./modules/books');
+
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get('/', (req, res) => res.status(200).send('Welcome!'));
 
-  response.send('test request received')
+app.get('/test', (req, res) => {
+  res.send('test request received')
+});
 
-})
+app.get('/books', getBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
